57120f80be0c17ad3f48177d9c0d7f5a7e94aae5
Syeda Fatema <farwafatema1@gmail.com>
2025-03-24 15:43:22 -0400

    Fixed build issues and updated Home component

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,15 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[40px] text-white font-semibold">
-          Shaping the Future of the Web with
+            Shaping the Future of the Web with
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               Innovative Designs
             </span>
           </h1>
           <p className="text-gray-200 text-[17px] hidden md:block">
-          I'm a Developer & Designer passionate about creating impactful experiences.
-          Explore my work to see how I blend creativity and technology to make a meaningful impact.
+            I&apos;m a Developer & Designer passionate about creating impactful experiences.
+            Explore my work to see how I blend creativity and technology to make a meaningful impact.
           </p>
           <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
@@ -33,14 +33,14 @@ export default function Home() {
               href="/my-projects"
               className="rounded-[20px] group relative bg-sky-800 hover:bg-sky-600 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               My projects
             </Link>
             <Link
               href="/contact-me"
               className="rounded-[20px] group relative bg-sky-800 hover:bg-sky-700 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               Contact me
             </Link>
           </div>
@@ -75,7 +75,7 @@ export default function Home() {
         alt="stars"
         height={1000}
         width={1000}
-        className="absolute top-10 bottom-70 left-100 right-50 z-[150]"
+        className="absolute top-10 left-[100px] right-[50px] z-[150]"
       />
     </main>
   );
